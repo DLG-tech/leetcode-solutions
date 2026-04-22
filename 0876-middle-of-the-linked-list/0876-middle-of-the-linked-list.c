564b44dafc239ebbff1ed80c97c9a2b33404c9d7
D-L-G-TECH <orionnthe1@gmail.com>
2026-04-21 20:17:53 -0400

    Time: 0 ms (100%), Space: 9.1 MB (67.84%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.c
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.c
@@ -6,21 +6,9 @@
  * };
  */
 struct ListNode* middleNode(struct ListNode* head) {
-    struct ListNode* slow = head;
-    struct ListNode* fast = head;
-    struct ListNode* midd = slow;
-    while (fast != NULL && fast->next != NULL) {
-    slow = slow->next;
-    fast = fast->next->next;
-
-}
-         printf("[");
-        while(midd != NULL){
-            printf("%d", midd->val);
-             if (midd->next != NULL) printf(",");
-             midd = midd->next;
-        }
-        printf("]");  
-
+    struct ListNode* slow = head, *fast = head;
+    while (fast && fast->next != NULL) {
+    slow = slow->next, fast = fast->next->next;
+    }
         return slow;
 }
